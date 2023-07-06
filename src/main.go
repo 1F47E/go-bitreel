8e762091e19542c7edcc9e43597942afccabaf51
kas <32327548+kaspar1ndustries@users.noreply.github.com>
2023-07-06 14:05:05 -0400

    multi frames proccessing, concurency

--- src/main.go
+++ src/main.go
@@ -8,9 +8,17 @@ import (
 	"image/png"
 	"io"
 	"log"
+	"math"
 	"os"
 	"path/filepath"
+	"sync"
 )
+
+// Amount of bits in 1 4k frame
+// 3840*2160/4 = 2073600
+const frameSizeBits = 2073600
+
+var wg sync.WaitGroup
 
 func main() {
 	// read cmd line args
@@ -27,6 +35,10 @@ func main() {
 	} else {
 		encode(file)
 	}
+
+	fmt.Println("Waiting for goroutines to finish")
+	wg.Wait()
+	fmt.Println("Done")
 }
 
 func decode(filename string) {
@@ -105,6 +117,7 @@ func encode(filename string) {
 	}
 	defer file.Close()
 
+	// TODO: stream file data, not copy to buffer
 	var buf bytes.Buffer
 	_, err = io.Copy(&buf, file)
 	if err != nil {
@@ -113,18 +126,59 @@ func encode(filename string) {
 
 	b := buf.Bytes()
 	fmt.Println("Read bytes:", len(b))
-	bits := make([]bool, len(b)*8)
-	for i := 0; i < len(b); i++ {
-		// for every byte convert it to 8 bits
+
+	// calc amount of frames
+	framesCnt := math.Ceil(float64(len(b)) / float64(frameSizeBits/8))
+	fmt.Println("Frames:", framesCnt)
+	// bitsBuffer := make([]bool, frameSizeBits)
+	var bitsBuffer [frameSizeBits]bool
+
+	bitIndex := 0
+	// for i := 0; i < len(b); i++ {
+	totalFrameBytes := int(framesCnt) * frameSizeBits / 8
+	fmt.Println("Frames bytes size:", totalFrameBytes)
+	// for f := 0; f < framesBytesSize; f++ {
+	// for every byte convert it to 8 bits
+
+	// range over all frames, more then file len!
+	for i := 0; i < totalFrameBytes; i++ {
 		for j := 0; j < 8; j++ {
-			bits[i*8+j] = (b[i] & (1 << uint(j))) != 0
+			frameNumber := i / (frameSizeBits / 8)
+			shift := frameNumber * frameSizeBits
+			bitIndex = i*8 + j - shift // should reset to 0 on every frame
+			// if we have more bytes than needed, fill the rest with 0
+			if i >= len(b) {
+				bitsBuffer[bitIndex] = false
+			} else {
+				bitsBuffer[bitIndex] = (b[i] & (1 << uint(j))) != 0
+			}
+
+			// detect the end of the file or the end of the frame
+			// proccess the image, save, reset the buffer
+			// send a copy of bits buffer to goroutine to proccess
+			// panic on errors - missed frames are not allowed
+			if bitIndex == len(bitsBuffer)-1 || bitIndex == len(b)*8-1 {
+				wg.Add(1)
+				go func(bitsBuffer [frameSizeBits]bool, fn int) {
+					defer wg.Done()
+					fmt.Println("Proccessing frame in G:", fn)
+					img := encodeFrame(bitsBuffer)
+					// prefix filename with leading zeroes
+					fileName := fmt.Sprintf("tmp/out_%09d.png", fn+1)
+					save(fileName, img)
+					fmt.Println("Frame done:", fn)
+				}(bitsBuffer, frameNumber)
+			}
 		}
+		// }
 	}
-	fmt.Println("bits done")
+	fmt.Println("Done")
+}
 
-	// TODO: encode frame in a func, allow many frames
-
+func encodeFrame(bits [frameSizeBits]bool) *image.NRGBA {
+	fmt.Println("Encoding frame")
 	// generate an image
+
 	img := image.NewNRGBA(image.Rect(0, 0, 3840, 2160)) // 4K resolution
 
 	// generate image
@@ -133,17 +187,21 @@ func encode(filename string) {
 	k := 0
 	for x := 0; x < img.Bounds().Dx(); x += 2 {
 		for y := 0; y < img.Bounds().Dy(); y += 2 {
-			var col color.Color
-			if k < len(bits) {
+			// var col color.Color
+			// set red color as default background
+			col := color.NRGBA{255, 0, 0, 255}
+			if k < len(bits) { // BUG: always true
 				if bits[k] {
-					col = color.Black
+					// col = color.Black
+					col = color.NRGBA{0, 0, 0, 255}
 				} else {
-					col = color.White
+					// col = color.White
+					col = color.NRGBA{255, 255, 255, 255}
 				}
 				k++
 			} else {
-				// red color as end
-				col = color.RGBA{255, 0, 0, 255}
+				col = color.NRGBA{255, 0, 0, 255}
+				fmt.Println("END")
 			}
 			// // generate random int
 			// if rand.Intn(2)%2 == 0 {
@@ -158,7 +216,9 @@ func encode(filename string) {
 			img.Set(x+1, y+1, col)
 		}
 	}
-	save("test.png", img)
+
+	fmt.Println("Encoding frame done")
+	return img
 }
 
 func save(filePath string, img *image.NRGBA) {
@@ -166,6 +226,11 @@ func save(filePath string, img *image.NRGBA) {
 	defer imgFile.Close()
 	if err != nil {
 		log.Println("Cannot create file:", err)
+		panic(fmt.Sprintf("Cannot create file: %s", err))
 	}
-	png.Encode(imgFile, img.SubImage(img.Rect))
+	err = png.Encode(imgFile, img.SubImage(img.Rect))
+	if err != nil {
+		log.Println("Cannot encode to file:", err)
+		panic(fmt.Sprintf("Cannot encode to file: %s", err))
+	}
 }
